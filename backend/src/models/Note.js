819b07ff39d503528d1b02d3c8ca5f33fa58dc9e
ttfamm <tfamm022@gmail.com>
2025-07-09 13:18:31 -0500

    initial commit

---
+++ backend/src/models/Note.js
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+
+}, { timestamps: true }) // createdAt updatedAt
+
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note
